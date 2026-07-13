e4ac6d34d1b3275ed284677d847f3af3636521b9
goldstr1ker <goldstr1ker>
2026-07-13 19:31:32 +0300

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain; charset=utf-8')
+            self.end_headers()
+            self.wfile.write("Hello from Effective Mobile!".encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+if __name__ == '__main__':
+    server_address = ('0.0.0.0', 8080)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print("Backend started on port 8080...")
+    httpd.serve_forever()
